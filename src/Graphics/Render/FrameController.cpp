3239e3e5dfb7294ee4f4991dd14fef0401dc3c0d
TheGoldenProof <firebirdspiral@gmail.com>
2023-12-23 22:23:02 +0000

    fix depth test and opacity issues

--- src/Graphics/Render/FrameController.cpp
+++ src/Graphics/Render/FrameController.cpp
@@ -43,32 +43,36 @@ void FrameController::Execute(Globe& gb) dbgexcept {
 	const auto currentCamName = gb.Cams().GetActiveCamera().transform([](const auto& camRef) { return camRef.get().GetName(); });
 	const bool depthTestEnabled = gfx.GetDepthTestEnabled();
 
-	constexpr f32 nearZ = 1.0f / 64;
+	constexpr f32 nearZ = 8.0f;
 	const f32 aspect = static_cast<f32>(gfx.GetWidth()) / static_cast<f32>(gfx.GetHeight());
-	auto proj = DirectX::XMMatrixPerspectiveFovLH(fov, aspect, nearZ, 8192.0f);
-	gb.Gfx().SetProjection(std::move(proj));
 	
 	gfx.BindSwapBuffer();
+
+	gb.Cams().SetActive("HUD cam");
+	gb.Cams().BindActive(gfx);
+	gfx.SetDepthTest(true, D3D11_COMPARISON_EQUAL, D3D11_DEPTH_WRITE_MASK_ZERO);
+	gfx.SetProjection(DXUtil::CustomOrthoProj(aspect, 1.0f));
+	passes[24].Execute(gfx);
+
+	auto proj = DirectX::XMMatrixPerspectiveFovLH(fov, aspect, nearZ, 8192.0f);
+	gb.Gfx().SetProjection(std::move(proj));
+	gfx.SetDepthTest(true);
+	if (currentCamName) gb.Cams().SetActive(currentCamName.value());
+	gb.Cams().BindActive(gfx);
 	//gfx.SetProjection(projF);
 	for (u32 i = 0; i < 16; i++) passes[i].Execute(gfx);
 
 	gb.Cams().SetActive("Camera1");
 	gb.Cams().BindActive(gfx);
-	//f32 projW = OMIDIV_WINW;
-	//const f32 projH = OMIDIV_WINH;
-	//if (aspect != static_cast<f32>(OMIDIV_WINW) / OMIDIV_WINH) {
-	//	projW = projH * aspect;
-	//}
-	//const f32 scale = 1.0f;// tan(fov / 2.0f);
-	//gfx.SetProjection(DXUtil::CustomOrthoProj(projW * scale, projH * scale));
-	gfx.SetProjection(DXUtil::CustomOrthoProj(aspect, 1.0f));
+	f32 projW = OMIDIV_WINW;
+	const f32 projH = OMIDIV_WINH;
+	if (aspect != static_cast<f32>(OMIDIV_WINW) / OMIDIV_WINH) {
+		projW = projH * aspect;
+	}
+	const f32 scale = 1.0f;// tan(fov / 2.0f);
+	gfx.SetProjection(DXUtil::CustomOrthoProj(projW * scale, projH * scale));
 	//gfx.SetProjection(DXUtil::CustomOrthoProj(OMIDIV_WINW, OMIDIV_WINH));
 	for (u32 i = 16; i < 24; i++) passes[i].Execute(gfx);
-
-	gb.Cams().SetActive("HUD cam");
-	gb.Cams().BindActive(gfx);
-	gfx.SetDepthTest(true, D3D11_COMPARISON_EQUAL, D3D11_DEPTH_WRITE_MASK_ZERO);
-	passes[24].Execute(gfx);
 
 	//gfx.SetProjection(DXUtil::CustomOrthoProj(static_cast<f32>(gfx.GetWidth()), static_cast<f32>(gfx.GetHeight())));
 	gfx.SetDepthTest(false);
